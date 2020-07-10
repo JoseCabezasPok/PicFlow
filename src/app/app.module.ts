b97aed922e5a60dac94a474f587380e3c1070330
JoseCabezasPok <jose.cabezas@campusdual.com>
2020-07-10 21:08:53 +0200

    navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import {HttpClientModule} from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ImageGalleryComponent } from './image-gallery/image-gallery.component';
+import { FinderComponent } from './finder/finder.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ImageGalleryComponent
+    ImageGalleryComponent,
+    FinderComponent
   ],
   imports: [
     BrowserModule,
